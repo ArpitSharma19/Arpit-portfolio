7f6dec1cec9e42fdf7ba730cc62059bc4822b528
ArpitSharma19 <arpit.atrii@gmail.com>
2025-09-09 16:43:49 +0530

    Completed

--- js/script.js
+++ js/script.js
@@ -1,10 +1,46 @@
+//toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+//Scroll section
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
 window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 100;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            //Active link
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+            //active animation
+            sec.classList.add('show-animate');
+        }
+        else{
+            sec.classList.remove('show-animate');
+        }
+    });
+
+    //Sticky header
     let header = document.querySelector('header');
     header.classList.toggle('sticky', window.scrollY > 100);
 
+    //Remove toggle
     menuIcon.classList.remove('bx-x');
     navbar.classList.remove('active');
 
+    //Animation footer
     let footer = document.querySelector('footer');
-    footer.classList.toggle('show', window.scrollY + window.innerHeight >= document.body.scrollHeight);
+    footer.classList.toggle('show-animate', this.innerHeight + this.scrollY >= document.scrollingElement.scrollHeight);
 }
